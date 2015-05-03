86a8f5791931132d4b14f7c31307b366e66e1777
Valjean <valjeanbud@gmail.com>
2015-05-03 13:10:29 -0700

    small update to bash post, remove route gen code from gulp blog task

--- gulp/blog.js
+++ gulp/blog.js
@@ -12,68 +12,22 @@ var JSONStream = require('JSONStream');
 
 module.exports = function(options, paths) {
 
+  // takes a string of the form yyyymmdd and turns it into a date object
+  function dateify(date) {
+    if(typeof date !== 'string' || date.length !== 8) {
+      // `date` is malformed, return the current time instead
+      return new Date();
+    }
+    var y, m, d;
+    y = date.substring(0,4);
+    m = date.substring(4,6);
+    d = date.substring(7);
+    return new Date(y, m, d);
+  }
+
   // for all the blog posts, strip out the front matter and copy remainder of content into markdown files in .tmp
   // take all the frontmatter from the posts, take date and route from post filename, and compile it into one big posts.json file
   gulp.task('blog', function() {
-
-    // takes a string of the form yyyymmdd and turns it into a date object
-    function dateify(date) {
-      if(typeof date !== 'string' || date.length !== 8) {
-        // `date` is malformed, return the current time instead
-        return new Date();
-      }
-      var y, m, d;
-      y = date.substring(0,4);
-      m = date.substring(4,6);
-      d = date.substring(7);
-      return new Date(y, m, d);
-    }
-    // var postsStream = gulp.src(paths.posts + '/*.md')
-    //   .pipe($.frontMatter())
-    //   // TODO: htmlify after transforming the stream
-    //   // htmlify sans frontmatter
-    //   .pipe($.markdown())
-    //   // copy posts html files
-    //   .pipe(gulp.dest(paths.tmpPosts))
-    //   // check the filename and parse it into the date and route
-    //   .pipe(through.obj(function(file, encoding, callback) {
-    //     var route, date;
-    //     // parse the filename into the route and date
-    //     var filename = file.relative.replace('.html', '').split('_');
-    //     // there should be exactly one underscore between two strings
-    //     if(filename.length === 2) {
-    //       // add the date and route from the filename to the frontmatter
-    //       file.frontMatter.date = filename[0];
-    //       file.frontMatter.route = filename[1];
-    //       // console.log(file.frontMatter);
-    //       this.push(file);
-    //     } else {
-    //       console.warn('Post file ' + file.relative + ' not named correctly.\nFile should be named "[date]_[route].md".\nExample: 20150101_happy-new-year.md');
-    //       this.push(null);
-    //     }
-    //     callback();
-    //   }))
-
-    // return gulp.src(paths.src + '/index.js')
-    //   .pipe($.inject(postsStream, {
-    //     starttag: '/* @injectRoutes start */',
-    //     endtag: '/* @injectRoutes end */',
-    //     transform: function(filepath, file) {
-    //       var route = [
-    //         '.state(\'blog/' + file.frontMatter.route + '\', {',
-    //         '  url: \'/blog/' + file.frontMatter.route + '\',',
-    //         '  templateUrl: \'' + 'posts/' + file.relative + '\'',
-    //         // '  controller: function() {',
-    //         // '    var vm = this;',
-    //         // '    vm.mdPath = \'' + 'posts/' + file.relative + '\';',
-    //         // '  },',
-    //         // '  controllerAs: \'vm\'',
-    //         '})'
-    //       ].join('\n');
-    //       return route;
-    //     }
-    //   }))
-    //   .pipe(gulp.dest(paths.src))
 
     // create the tmp posts directory if it does not already exist
     // TODO: should do this at the last pipe below where we fs.createWriteStream
--- posts/20150427_auto-proxy-bash.md
+++ posts/20150427_auto-proxy-bash.md
@@ -8,9 +8,11 @@ tags:
 - panda
 ---
 
-If you use OS X at work, you may have a similar issue.
+I use OS X at work, and unfortunately the best network and client configuration is done for Windows. OS X users are second-class citizens (Linux/Unix users have it even worse).
 
-When I am on my work's network, I have to have Auto Proxy Discovery turned on to browse websites outside of the intranet. It is a 6-step process to do this from the UI:
+When I am on my work's network, I must have Auto Proxy Discovery turned on in order to browse websites outside of the intranet. I would leave it on all the time, but then I have to wait for my browsers to attempt the proxy discovery when I am outside of my work network. 
+
+You can toggle Auto Proxy Discovery in System Preferences. It is a 6-step process:
 
 1. Go to the Network section of System Preferences
 2. Open the Advanced settings of the Wi-Fi interface
@@ -39,7 +41,7 @@ function autoproxtog() {
 
 The above function is simple to use and will toggle your Auto Proxy Discovery state on the network interface named "Wi-Fi". If you want to use it on a different interface, simply replace "Wi-Fi" with name of the interface you want to toggle (e.g. "USB Ethernet").
 
-To use it, add the above code into your `.bash_profile`, then reload your `.bash_profile` with `source ~./bash_profile` or by opening up a new Terminal tab/window.
+To use it, add the above code to your `.bash_profile`, then reload your `.bash_profile` with `source ~./bash_profile` or by opening up a new Terminal tab/window.
 
 Then you can run the function by simply typing:
 ```
